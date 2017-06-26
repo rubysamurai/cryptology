a3929722c089c6871de9afb91d3199f35a883997
Dmitriy Tarasov <info@rubysamurai.com>
2017-06-26 17:34:41 +0300

    Always set iv

--- lib/cryptology.rb
+++ lib/cryptology.rb
@@ -4,8 +4,11 @@ require 'base64'
 
 module Cryptology
   def self.encrypt(data:, key:, cipher: 'AES-256-CBC', iv: nil)
+    iv ||= OpenSSL::Cipher.new(cipher).random_iv
     encrypted = encrypt_data(data.to_s, digest(key), cipher, iv)
-    ::Base64.encode64(encrypted)
+    { 'cipher' => cipher,
+      'iv' => iv,
+      'data' => ::Base64.encode64(encrypted) }
   end
 
   def self.decrypt(data:, key:, cipher: 'AES-256-CBC', iv: nil)
@@ -21,23 +24,23 @@ module Cryptology
   end
 
   def self.encrypt_data(data, key, cipher, iv)
-    cipher = ::OpenSSL::Cipher.new(cipher)
+    cipher = OpenSSL::Cipher.new(cipher)
     cipher.encrypt
     cipher.key = key
-    cipher.iv  = iv unless iv.nil?
+    cipher.iv  = iv unless iv.nil? || iv.length != 16
     cipher.update(data) + cipher.final
   end
 
   def self.decrypt_data(data, key, cipher, iv)
-    decipher = ::OpenSSL::Cipher.new(cipher)
+    decipher = OpenSSL::Cipher.new(cipher)
     decipher.decrypt
     decipher.key = key
-    decipher.iv  = iv unless iv.nil?
+    decipher.iv  = iv unless iv.nil? || iv.length != 16
     decipher.update(data) + decipher.final
   end
 
   def self.digest(key)
-    ::OpenSSL::Digest::SHA256.digest(key)
+    OpenSSL::Digest::SHA256.digest(key)
   end
 
   private_class_method :encrypt_data, :decrypt_data, :digest
--- spec/cryptology_spec.rb
+++ spec/cryptology_spec.rb
@@ -20,7 +20,11 @@ describe Cryptology do
     data      = 'Very confidential data with UTF-8 symbols: ♠ я ü æ'
     key       = 'veryLongAndSecurePassword_61543054534'
     encrypted = Cryptology.encrypt(data: data, key: key)
-    expect(Cryptology.decrypt(data: encrypted, key: key)).to eq data
+    expect(
+      Cryptology.decrypt(data: encrypted['data'],
+                         key: key,
+                         iv: encrypted['iv'])
+    ).to eq data
   end
 
   it 'encrypts and decrypts with iv argument' do
@@ -28,7 +32,12 @@ describe Cryptology do
     key       = 'veryLongAndSecurePassword_61543054534'
     iv        = OpenSSL::Cipher.new('AES-256-CBC').random_iv
     encrypted = Cryptology.encrypt(data: data, key: key, iv: iv)
-    expect(Cryptology.decrypt(data: encrypted, key: key, iv: iv)).to eq data
+    expect(encrypted['iv']).to eq iv
+    expect(
+      Cryptology.decrypt(data: encrypted['data'],
+                         key: key,
+                         iv: iv)
+    ).to eq data
   end
 
   it 'encrypts and decrypts with cipher argument' do
@@ -36,14 +45,19 @@ describe Cryptology do
     key       = 'veryLongAndSecurePassword_61543054534'
     cipher    = 'CAMELLIA-256-CBC'
     encrypted = Cryptology.encrypt(data: data, key: key, cipher: cipher)
-    expect(Cryptology.decrypt(data: encrypted, key: key, cipher: cipher))
-      .to eq data
+    expect(encrypted['cipher']).to eq cipher
+    expect(
+      Cryptology.decrypt(data: encrypted['data'],
+                         key: key,
+                         cipher: cipher,
+                         iv: encrypted['iv'])
+    ).to eq data
   end
 
   it 'throws error without data argument' do
     key    = 'veryLongAndSecurePassword_61543054534'
     cipher = 'AES-256-CBC'
-    iv     = OpenSSL::Cipher.new('AES-256-CBC').random_iv
+    iv     = OpenSSL::Cipher.new(cipher).random_iv
     expect { Cryptology.encrypt(key: key, cipher: cipher, iv: iv) }
       .to raise_error(ArgumentError, 'missing keyword: data')
   end
@@ -51,28 +65,52 @@ describe Cryptology do
   it 'throws error without key argument' do
     data   = 'Very confidential data with UTF-8 symbols: ♠ я ü æ'
     cipher = 'AES-256-CBC'
-    iv     = OpenSSL::Cipher.new('AES-256-CBC').random_iv
+    iv     = OpenSSL::Cipher.new(cipher).random_iv
     expect { Cryptology.encrypt(data: data, cipher: cipher, iv: iv) }
       .to raise_error(ArgumentError, 'missing keyword: key')
   end
 
   context '#decryptable?' do
+    let(:data) { 'Very confidential data with UTF-8 symbols: ♠ я ü æ' }
+    let(:key) { 'veryLongAndSecurePassword_61543054534' }
+    let(:cipher) { 'AES-256-CBC' }
+    let(:iv) { OpenSSL::Cipher.new(cipher).random_iv }
+    let(:encrypted) { Cryptology.encrypt(data: data, key: key, cipher: cipher, iv: iv) }
+
     it 'returns true for valid arguments' do
-      data      = 'Very confidential data with UTF-8 symbols: ♠ я ü æ'
-      key       = 'veryLongAndSecurePassword_61543054534'
-      iv        = OpenSSL::Cipher.new('AES-256-CBC').random_iv
-      encrypted = Cryptology.encrypt(data: data, key: key, iv: iv)
-      expect(Cryptology.decryptable?(data: encrypted, key: key, iv: iv))
-        .to be true
+      expect(
+        Cryptology.decryptable?(data: encrypted['data'],
+                                key: key,
+                                cipher: cipher,
+                                iv: iv)
+      ).to be true
     end
 
-    it 'returns false for invalid arguments' do
-      data      = 'Very confidential data with UTF-8 symbols: ♠ я ü æ'
-      key       = 'veryLongAndSecurePassword_615430'
-      iv        = OpenSSL::Cipher.new('AES-256-CBC').random_iv
-      encrypted = Cryptology.encrypt(data: data, key: key, iv: iv)
-      expect(Cryptology.decryptable?(data: encrypted, key: 'a' * 32, iv: iv))
-        .to be false
+    it 'returns false for invalid data argument' do
+      expect(
+        Cryptology.decryptable?(data: 'random',
+                                key: key,
+                                cipher: cipher,
+                                iv: iv)
+      ).to be false
+    end
+
+    it 'returns false for invalid key argument' do
+      expect(
+        Cryptology.decryptable?(data: encrypted['data'],
+                                key: key.upcase,
+                                cipher: cipher,
+                                iv: iv)
+      ).to be false
+    end
+
+    it 'returns false for invalid cipher argument' do
+      expect(
+        Cryptology.decryptable?(data: encrypted['data'],
+                                key: key,
+                                cipher: 'CAMELLIA-256-CBC',
+                                iv: iv)
+      ).to be false
     end
   end
 
@@ -95,7 +133,7 @@ describe Cryptology do
         key       = OpenSSL::Cipher.new(c).random_key
         iv        = OpenSSL::Cipher.new(c).random_iv
         encrypted = Cryptology.encrypt(data: data, key: key, cipher: c, iv: iv)
-        expect(Cryptology.decrypt(data: encrypted, key: key, cipher: c, iv: iv))
+        expect(Cryptology.decrypt(data: encrypted['data'], key: key, cipher: c, iv: iv))
           .to eq data
       end
     end
