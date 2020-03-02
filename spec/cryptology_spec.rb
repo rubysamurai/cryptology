eaf48e3ba8c89012ae4f0db9efa5fc1622a17b71
Dmitriy Tarasov <info@rubysamurai.com>
2020-03-03 00:20:23 +0300

    test: update test ciphers list

--- spec/cryptology_spec.rb
+++ spec/cryptology_spec.rb
@@ -1,17 +1,35 @@
-CIPHERS = %w[AES-256-CBC
+CIPHERS = %w[AES-128-XTS
+             AES-256-CBC
+             AES-256-CBC-HMAC-SHA1
+             AES-256-CBC-HMAC-SHA256
              AES-256-CFB
              AES-256-CFB1
              AES-256-CFB8
              AES-256-CTR
              AES-256-ECB
              AES-256-OFB
+             AES256
+
+             ARIA-256-CBC
+             ARIA-256-CFB
+             ARIA-256-CFB1
+             ARIA-256-CFB8
+             ARIA-256-CTR
+             ARIA-256-ECB
+             ARIA-256-OFB
+             ARIA256
 
              CAMELLIA-256-CBC
              CAMELLIA-256-CFB
              CAMELLIA-256-CFB1
              CAMELLIA-256-CFB8
+             CAMELLIA-256-CTR
              CAMELLIA-256-ECB
-             CAMELLIA-256-OFB].freeze
+             CAMELLIA-256-OFB
+             CAMELLIA256
+
+             CHACHA20
+             CHACHA20-POLY1305].freeze
 
 RSpec.describe Cryptology do
   let(:data) { 'Very confidential data with UTF-8 symbols: ♠ я ü æ' }
